3dc44bdbcc7d319a59e8a716cec784888f8f0a30
ManavR123 <manav.rathod@berkeley.edu>
2020-11-20 02:25:50 -0500

    experiments

---
+++ src/experiments.sh
@@ -0,0 +1,8 @@
+bash finetune.sh squad_gold_train
+bash predict.sh squad_gold_test
+
+bash finetune.sh synqg_train
+bash predict.sh synqg_test
+
+bash finetune.sh combined_gold_train
+bash predict.sh combined_gold_test
