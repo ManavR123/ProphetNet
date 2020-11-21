54d1e4d98d5fd830ae9ff71410ab869dacf1091c
ManavR123 <manav.rathod@berkeley.edu>
2020-11-21 15:21:14 -0500

    cleaned up data; updated finetune process to use validation

--- src/finetune.sh
+++ src/finetune.sh
@@ -1,12 +1,16 @@
 MAIN_DIR=qg/${1}
 
 echo "Tokenizing data"
-python data_tokenize.py --input ${MAIN_DIR}/${1}.txt
-python data_tokenize.py --input ${MAIN_DIR}/${1}_q.txt
+python data_tokenize.py --input train_${MAIN_DIR}/${1}.txt
+python data_tokenize.py --input train_${MAIN_DIR}/${1}_q.txt
+python data_tokenize.py --input valid_${MAIN_DIR}/${1}.txt
+python data_tokenize.py --input valid_${MAIN_DIR}/${1}_q.txt
 
 #echo "Generating src and tgt files"
-python process_data.py --input ${MAIN_DIR}/tokenized_${1}.txt --output ${MAIN_DIR}/${1}.src --src
-python process_data.py --input ${MAIN_DIR}/tokenized_${1}_q.txt --output ${MAIN_DIR}/${1}.tgt --tgt
+python process_data.py --input ${MAIN_DIR}/tokenized_train_${1}.txt --output ${MAIN_DIR}/train_${1}.src --src
+python process_data.py --input ${MAIN_DIR}/tokenized_train_${1}_q.txt --output ${MAIN_DIR}/train_${1}.tgt --tgt
+python process_data.py --input ${MAIN_DIR}/tokenized_valid_${1}.txt --output ${MAIN_DIR}/valid_${1}.src --src
+python process_data.py --input ${MAIN_DIR}/tokenized_valid_${1}_q.txt --output ${MAIN_DIR}/valid_${1}.tgt --tgt
 
 echo "Fairseq Preprocess"
 DEST_DIR=qg/${1}/processed
@@ -15,8 +19,8 @@ fairseq-preprocess \
 --user-dir prophetnet \
 --task translation_prophetnet \
 --source-lang src --target-lang tgt \
---trainpref qg/${1}/${1} \
---validpref qg/${1}/${1} \
+--trainpref qg/${1}/train_ \
+--validpref qg/${1}/valid_ \
 --destdir $DEST_DIR --srcdict vocab.txt --tgtdict vocab.txt \
 --workers 20
 
@@ -31,17 +35,16 @@ PRETRAINED_MODEL=qg/pretrained_checkpoints/prophetnet_large_16G_qg_model.pt
 fairseq-train \
 --user-dir $USER_DIR --task translation_prophetnet --arch $ARCH \
 --optimizer adam --adam-betas '(0.9, 0.999)' --clip-norm 0.1 \
---lr 0.00001 --min-lr 1e-09 \
+--lr 0.000001 --min-lr 1e-09 \
 --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 1000 \
 --dropout 0.1 --attention-dropout 0.1 --weight-decay 0.01 \
 --criterion $CRITERION --label-smoothing 0.1 \
---update-freq 1  --max-tokens 1400 --max-sentences 7 \
+--update-freq 1  --max-tokens 1400 --max-sentences 1 \
 --load-from-pretrained-model $PRETRAINED_MODEL \
---ddp-backend=no_c10d --max-epoch 10 \
+--ddp-backend=no_c10d --max-epoch 3 \
 --max-source-positions 512 --max-target-positions 512 \
 --skip-invalid-size-inputs-valid-test \
 --save-dir $SAVE_DIR \
---keep-last-epochs 10 \
 --tensorboard-logdir $TENSORBOARD_LOGDIR \
 $DEST_DIR
 
--- src/qg/finetune_process.py
+++ src/qg/finetune_process.py
@@ -2,27 +2,41 @@ import argparse
 import os
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
 
 def create_input(dirname):
     df = pd.read_csv(os.path.join(dirname, f"{dirname}.csv"))
     paras = []
     qs = []
     for index, row in df.iterrows():
-        paras.append(row["context"].replace("\n", "") + ' [SEP] ' + row["answer"])
+        paras.append(row["context"].replace("\n", "") + " [SEP] " + row["answer"])
         qs.append(row["question"])
 
-    f1 = open(os.path.join(dirname, f"{dirname}.txt"), "w", encoding="utf-8")
-    f2 = open(os.path.join(dirname, f"{dirname}_q.txt"), "w", encoding="utf-8")
-    f1.write('\n'.join(paras))
-    f2.write('\n'.join(qs))
+    train_paras, train_qs, valid_paras, valid_qs = train_test_split(
+        paras, qs, test_size=0.25, random_state=42
+    )
+
+    f1 = open(os.path.join(dirname, f"train_{dirname}.txt"), "w", encoding="utf-8")
+    f2 = open(os.path.join(dirname, f"train_{dirname}_q.txt"), "w", encoding="utf-8")
+    f1.write("\n".join(train_paras))
+    f2.write("\n".join(train_qs))
+    f1.close()
+    f2.close()
+
+    f1 = open(os.path.join(dirname, f"valid_{dirname}.txt"), "w", encoding="utf-8")
+    f2 = open(os.path.join(dirname, f"valid_{dirname}_q.txt"), "w", encoding="utf-8")
+    f1.write("\n".join(valid_paras))
+    f2.write("\n".join(valid_qs))
     f1.close()
     f2.close()
 
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Processing script for causal question experiments')
-    parser.add_argument('--dirname', type=str, help='Name of directory to process')
+    parser = argparse.ArgumentParser(
+        description="Processing script for causal question experiments"
+    )
+    parser.add_argument("--dirname", type=str, help="Name of directory to process")
     args = parser.parse_args()
     if args.dirname:
         create_input(args.dirname)
